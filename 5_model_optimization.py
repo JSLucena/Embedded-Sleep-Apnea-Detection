Fixed lack of quantized IO on int8 model, both on tranining and inference. Added a simple calculation for AHI

5_model_optimization.py
@@ -35,21 +35,34 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', None)
 
 
-def evaluate_tflite_metrics(interpreter, x_test, y_test, threshold=0.5):
+def evaluate_tflite_metrics(interpreter, x_test, y_test, threshold=0.5, convert=False):
     input_details = interpreter.get_input_details()[0]
     output_details = interpreter.get_output_details()[0]
-    
-    y_true = []
     y_pred = []
+    y_true = []
+    
+    if convert:
+        input_scale, input_zero_point = input_details['quantization']
+        output_scale, output_zero_point = output_details['quantization']
+        print("Input scale/zero_point:", input_scale, input_zero_point)
+        print("Output scale/zero_point:", output_scale, output_zero_point)
     
     for i in range(len(x_test)):
-        # Prepare input data with type checking
-        input_data = np.expand_dims(x_test[i], axis=0).astype(input_details['dtype'])
+        input_data = np.expand_dims(x_test[i], axis=0)
+
+        if convert:
+            input_data = (input_data / input_scale + input_zero_point).astype(np.uint8)
+        else:
+            input_data = input_data.astype(input_details['dtype'])
+
         interpreter.set_tensor(input_details['index'], input_data)
         interpreter.invoke()
-        
-        # Get prediction (assuming single output with sigmoid activation)
+
         pred = interpreter.get_tensor(output_details['index'])
+
+        if convert:
+            pred = (pred.astype(np.float32) - output_zero_point) * output_scale
+
         y_pred.append(pred[0][0])
         y_true.append(y_test[i][0])
     
@@ -331,7 +344,12 @@ q_aware_interpreter = tf.lite.Interpreter(model_content=q_aware_tflite_model)
 q_aware_interpreter.allocate_tensors()
 print("baseline", evaluate_tflite_metrics(interpreter, X_test, y_test))
 print("dynamic", evaluate_tflite_metrics(interpreter_post, X_test, y_test))
-print("full-int", evaluate_tflite_metrics(interpreter_int, X_test, y_test))
+
+# Quantize manually
+# Extract scale and zero point for input/output
+
+
+print("full-int", evaluate_tflite_metrics(interpreter_int, X_test, y_test, convert=True))
 print("float16", evaluate_tflite_metrics(interpreter_float, X_test, y_test))
 print("Act16-weight8", evaluate_tflite_metrics(interpreter_int2, X_test, y_test))
 print("q-aware", evaluate_tflite_metrics(q_aware_interpreter, X_test, y_test))

a.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 # Load your TFLite model
-model_path = 'models/q-aware.tflite'
+model_path = 'models/int8.tflite'
 interpreter = tf.lite.Interpreter(model_path=model_path)
 
 input_details = interpreter.get_input_details()

serial_control.py
@@ -214,7 +214,8 @@ def main():
             print("2. Send sample from feather file")
             print("3. Enter float values manually")
             print("4. Run Complete Benchmark")
-            print("5. Quit")
+            print("5. Change pico clock frequency")
+            print("6. Quit")
             
             choice = input("Enter choice (1-4): ").strip()
             
@@ -263,7 +264,16 @@ def main():
                 end_idx = 600
 
                 # Slice only the desired rows
-                subset_df = df.iloc[start_idx:end_idx]
+                #subset_df = df.iloc[start_idx:end_idx]
+                subset_df = df
+                min_threshold = 0
+                max_threshold = 0
+                last = False
+                patient = args.feather
+                if "inference1" in patient:
+                    sleep_length = 6.4
+                else:
+                    sleep_length = 6.7
                 for idx, row in tqdm.tqdm(subset_df.iterrows(), total=len(subset_df)):
                     sample = row['Segment'].astype(np.float32)
                     pico.send_float_array(sample)
@@ -274,6 +284,20 @@ def main():
                         # Extract float from string like "Result0.56" or "Result: 0.56"
                         pred_str = response.split()[-1]  # or use regex
                         pred = float(pred_str)
+
+                        if pred > 0.5:
+                            apnea = True
+                        else:
+                            apnea = 0
+
+                        if apnea == True and last == False:
+                            min_threshold += 1
+                            max_threshold += 1
+                        elif apnea == True and last == True:
+                            max_threshold += 1
+
+
+                        last = apnea
                         lat = lat.split()[-1]
                         lat = int(lat)
                     except Exception as e:
@@ -292,8 +316,13 @@ def main():
                 print(metrics)
                 latency /= len(subset_df)
                 print("Average Latency: " , latency)
+                print("Minimum AHI:" , min_threshold / sleep_length, " Maximum AHI:" , max_threshold / sleep_length)
 
-            elif choice == '5':
+            elif choice == '6':
+                break
+
+
+            elif choice == '6':
                 break
             
             else: